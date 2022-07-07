6b63ceedf3e6b8220a87ad0e707ede02174542e5
Riccardo Trivellato <6997999+rtrive@users.noreply.github.com>
2022-07-07 15:37:29 +0200

    chore: expense struct

--- expenses.go
+++ expenses.go
@@ -28,6 +28,19 @@ type Expenses interface {
 	CreateExpenseByShare(ctx context.Context, expense ExpenseByShare) ([]Expense, error)
 }
 
+type ActionBy struct {
+	Id                 uint32 `json:"id"`
+	FirtsName          string `json:"first_name"`
+	LastName           string `json:"last_name"`
+	Email              string `json:"email"`
+	RegistrationStatus string `json:"registration_status"`
+	Picture            struct {
+		Small  string `json:"small"`
+		Medium string `json:"medium"`
+		Large  string `json:"large"`
+	} `json:"picture"`
+}
+
 type Expense struct {
 	Cost           string `json:"cost"`
 	Description    string `json:"description"`
@@ -49,6 +62,59 @@ type ExpenseByShare struct {
 	ByShare map[string]interface{}
 }
 
+type ExpenseResponse struct {
+	Expense
+	Id                     uint32   `json:"id"`
+	FriendshipId           uint32   `json:"friendship_id"`
+	Repeats                bool     `json:"repeats"`
+	EmailReminder          bool     `json:"email_reminder"`
+	EmailReminderInAdvance uint8    `json:"email_reminder_in_advance"`
+	NextRepeat             string   `json:"next_repeat"`
+	CommentsCount          string   `json:"comments_count"`
+	Payment                string   `json:"payment"`
+	Transactionconfirmed   string   `json:"transaction_confirmed"`
+	CreatedAt              string   `json:"created_at"`
+	CreatedBy              ActionBy `json:"created_by"`
+	UpdatedAt              string   `json:"updated_at"`
+	UpdatedBy              ActionBy `json:"updated_by"`
+	DeletedAt              string   `json:"deleted_at"`
+	DeletedBy              ActionBy `json:"deleted_by"`
+	Repayments             []struct {
+		From   uint32 `json:"from"`
+		To     uint32 `json:"to"`
+		Amount string `json:"amount"`
+	} `json:"repayments"`
+	Category struct {
+		Id   uint32 `json:"id"`
+		Name string `json:"Name"`
+	} `json:"category"`
+	Receipt struct {
+		Large    string `json:"large"`
+		Original string `json:"original"`
+	} `json:"receipt"`
+	Users []struct {
+		User struct {
+			Id        uint32 `json:"id"`
+			FirtsName uint32 `json:"first_name"`
+			LastName  uint32 `json:"last_name"`
+			Picture   uint32 `json:"picture"`
+		} `json:"user"`
+		UserId     uint32 `json:"user_id"`
+		PaidShare  string `json:"paid_share"`
+		OwedShare  string `json:"owed_share"`
+		NetBalance string `json:"net_balance"`
+	} `json:"users"`
+	Comments []struct {
+		Id           uint32 `json:"id"`
+		Content      string `json:"content"`
+		CommentType  string `json:"comment_type"`
+		RelationType string `json:"relation_type"`
+		RelationId   uint32 `json:"relation_id"`
+		CreatedAt    string `json:"created_at"`
+		DeletedAt    string `json:"deleted_at"`
+		User         string `json:"user"`
+	} `json:"comments"`
+}
 type createExpenseResponse struct {
 	Expenses []Expense `json:"expenses"`
 }
